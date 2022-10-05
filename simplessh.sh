90991b998ef6ebd0bac89e342370477b2be6e099
HIdeSSH <102421846+hidessh99@users.noreply.github.com>
2022-10-05 10:25:46 +0700

    Update simplessh.sh

--- simplessh.sh
+++ simplessh.sh
@@ -203,6 +203,23 @@ wget -O /etc/default/sslh "https://gitlab.com/hidessh/baru/-/raw/main/SSLH/sslh.
 service sslh restart
 
 
+# setting vnstat
+apt -y install vnstat
+/etc/init.d/vnstat restart
+apt -y install libsqlite3-dev
+wget https://humdi.net/vnstat/vnstat-2.6.tar.gz
+tar zxvf vnstat-2.6.tar.gz
+cd vnstat-2.6
+./configure --prefix=/usr --sysconfdir=/etc && make && make install
+cd
+vnstat -u -i $NET
+sed -i 's/Interface "'""eth0""'"/Interface "'""$NET""'"/g' /etc/vnstat.conf
+chown vnstat:vnstat /var/lib/vnstat -R
+systemctl enable vnstat
+/etc/init.d/vnstat restart
+rm -f /root/vnstat-2.6.tar.gz
+rm -rf /root/vnstat-2.6
+
 # install squid
 #cd
 #apt -y install squid3
