0646664548ac0826cb94c32df1308785f65626fb
HIdeSSH <102421846+hidessh99@users.noreply.github.com>
2022-10-11 22:33:36 +0700

    Update fix-websocket.sh

--- fix-websocket.sh
+++ fix-websocket.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 # ==========================================
 
-sleep 20
-systemctl stop ws-tls 
-pkill python
-systemctl stop sslh
+#pkill python
+pkill python ws-tls 
 systemctl daemon-reload
+systemctl stop ws-tls 
+systemctl stop sslh
+
 systemctl disable ws-tls
 systemctl disable sslh
 systemctl daemon-reload
+
 systemctl enable sslh
 systemctl enable ws-tls
+
 systemctl start sslh 
 /etc/init.d/sslh start 
 /etc/init.d/sslh restart 
 systemctl start ws-tls
 systemctl restart ws-tls
+
 sleep 20
-restart
