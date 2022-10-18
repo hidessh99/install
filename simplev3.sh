de3acf74a51776c4e4edbe1c28dae8619b61852b
HIdeSSH <102421846+hidessh99@users.noreply.github.com>
2022-10-18 07:58:48 +0700

    Update simplev3.sh

--- simplev3.sh
+++ simplev3.sh
@@ -106,3 +106,57 @@ echo "30 * * * * root removelog" >> /etc/crontab
 echo "0 5 * * * root clear-log && reboot" >> /etc/crontab
 echo "0 17 * * * root clear-log && reboot" >> /etc/crontab
 echo "50 * * * * root userdelexpired" >> /etc/crontab
+
+
+echo "installing insaller l2tp/IPsec"
+echo "Progress..."
+echo "Sedang berlangsung..."
+sleep 3
+echo "silahkan dibaca, proses pemasangan semua script memakan waktu paling lama 10 menit sampai 30 menit"
+echo "Jika masih dalam tahap instalasi..."
+echo "jangan keluar dari terminal atau aplikasi"
+echo -e "[ ${green}INFO${NC} ] in the process of installing tools"
+echo -e "[ ${green}INFO${NC} ] dalam proses pemasangan alat"
+sleep 1
+wget https://raw.githubusercontent.com/hidessh99/projectku/main/ipsec/hidesetup-ipsec.sh && chmod +x hidesetup-ipsec.sh && ./hidesetup-ipsec.sh
+sleep 1
+echo "Download package tambahan untuk l2tp/ipsec"
+wget -O /usr/local/bin/stdev-l2tp-get-psk "https://raw.githubusercontent.com/hidessh99/projectku/main/ipsec/stdev-l2tp-get-psk"
+wget -O /usr/local/bin/stdev-l2tp-add-user "https://raw.githubusercontent.com/hidessh99/projectku/main/package-tambahan/stdev-l2tp-add-user"
+wget -O /usr/local/bin/stdev-l2tp-remove-user "https://raw.githubusercontent.com/hidessh99/projectku/main/package-tambahan/stdev-l2tp-remove-user"
+
+echo "permission file ipsec/l2tp"
+chmod +x /usr/local/bin/stdev-l2tp-get-psk 
+chmod +x /usr/local/bin/stdev-l2tp-add-user
+chmod +x /usr/local/bin/stdev-l2tp-remove-user
+
+
+sleep 1
+echo -e "[ ${green}INFO${NC} ] DONE... ALAT"
+sleep 1
+echo "Progress..."
+echo "Sedang berlangsung..."
+sleep 3
+
+
+echo "installing insaller wireguard"
+echo "Progress..."
+echo "Sedang berlangsung..."
+sleep 3
+echo "silahkan dibaca, proses pemasangan semua script memakan waktu paling lama 10 menit sampai 30 menit"
+echo "Jika masih dalam tahap instalasi..."
+echo "jangan keluar dari terminal atau aplikasi"
+echo -e "[ ${green}INFO${NC} ] in the process of installing tools"
+echo -e "[ ${green}INFO${NC} ] dalam proses pemasangan alat"
+sleep 1
+wget https://raw.githubusercontent.com/hidessh99/projectku/main/wireguard/hidewp-setup.sh && chmod +x hidewp-setup.sh && ./hidewp-setup.sh
+echo "Menajemen wireguard"
+sleep 1
+wget -O install-wireguard "https://www.dropbox.com/s/p89ubjpypmk26rv/install-wireguard?dl=1" && chmod +x install-wireguard && ./install-wireguard
+sleep 1
+cd
+echo -e "[ ${green}INFO${NC} ] DONE... ALAT"
+sleep 1
+echo "Progress..."
+echo "Sedang berlangsung..."
+sleep 3
